598c6849479b645f5e8764e2906192ad95135371
Rasel Hasan <raselhasanshovo@gmail.com>
2026-07-15 14:24:46 +0600

    Fixed live server currentUser runtime error 9

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 module.exports.sendEmail = async (userEmail, otpCode) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    
+    secure: true, // সিকিউর কানেকশন
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
